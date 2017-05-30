4af819d1733d65f795204c96e9141673bf3d6d13
Joyce <jcyang@hmc.edu>
2017-05-30 10:52:58 -0400

    First commit

---
+++ src/yournameis/StringAssignment.java
@@ -0,0 +1,19 @@
+
+package yournameis;
+import java.util.Scanner;
+public class StringAssignment
+{
+    public static void main (String[] args)
+    {
+    System.out.println("Enter your name: ");
+    Scanner receiveinput= new Scanner(System.in);
+    String name =receiveinput.next();    
+    
+    System.out.println("Your name is "+name);
+
+    }
+
+
+
+
+}
